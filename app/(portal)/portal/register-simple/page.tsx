d1e4dd188cf1729acd16f9ddfbc9eba29129f1c0
meryemnibras <nibrase12@gmail.com>
2025-12-18 16:54:50 +0300

    Add simple test page for register route debugging

---
+++ app/(portal)/portal/register-simple/page.tsx
@@ -0,0 +1,10 @@
+export default function SimpleRegisterPage() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Simple Register Page Test</h1>
+      <p>If you see this, the route is working!</p>
+      <p>Path: /portal/register-simple</p>
+    </div>
+  );
+}
+
